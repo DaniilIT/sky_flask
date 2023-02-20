a958f259774b2744dcda4ddab717bdbd52a386c6
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-02-20 13:09:51 +0100

    step 4: implement view user-feed

--- blueprints/main/views.py
+++ blueprints/main/views.py
@@ -6,12 +6,16 @@ main_blueprint = Blueprint('main_blueprint', __name__, template_folder='template
 
 @main_blueprint.route('/')
 def index_page():
+    """ Представление ленты постов
+    """
     posts = get_posts_all()
     return render_template('index.html', posts=posts)
 
 
 @main_blueprint.route('/posts/<int:postid>')
 def posts_page(postid):
+    """ Представление поста по идентификатору
+    """
     post = get_posts_by_pk(postid)
     comments = get_comments_by_post_id(postid)
     return render_template('post.html', post=post, comments=comments)
@@ -19,6 +23,16 @@ def posts_page(postid):
 
 @main_blueprint.route('/search/', methods=['GET'])
 def search_page():
+    """ Представление поиска постов по ключевому слову
+    """
     s = request.args.get('s')
     query_posts = search_for_posts(s)[:10]
     return render_template('search.html', posts=query_posts)
+
+
+@main_blueprint.route('/users/<username>', methods=['GET'])
+def users_page(username):
+    """ Представление постов пользователя
+    """
+    user_posts = get_posts_by_user(username)
+    return render_template('user-feed.html', posts=user_posts)
