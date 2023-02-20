663e52accc97410236b8d4eb42cd116619b5fb40
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-02-20 08:58:20 +0100

    step 3: implement view search

--- blueprints/main/views.py
+++ blueprints/main/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from utils import *
 
 
@@ -15,3 +15,10 @@ def posts_page(postid):
     post = get_posts_by_pk(postid)
     comments = get_comments_by_post_id(postid)
     return render_template('post.html', post=post, comments=comments)
+
+
+@main_blueprint.route('/search/', methods=['GET'])
+def search_page():
+    s = request.args.get('s')
+    query_posts = search_for_posts(s)[:10]
+    return render_template('search.html', posts=query_posts)
