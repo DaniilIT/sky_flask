4996e8646dc1bce8aebe380951846bd611e84e1e
Daniil_Manuilov <manuilovdaniil@yandex.ru>
2023-02-20 13:34:42 +0100

    step 5: implement views 404 and 500 error handler

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ from blueprints.main.views import main_blueprint
 app = Flask(__name__)
 app.register_blueprint(main_blueprint)
 
+
+@app.errorhandler(404)  # можно вызвать `abort(413)`
+def error404_page(error):
+    return '<h1 style="color: red;">Error 404 :(</h1><p>Страница не найдена</p>', 404
+
+
+@app.errorhandler(500)
+def error500_page(error):
+    return '<h1 style="color: red;">Error 500 :(</h1><p>Внутренняя ошибка сервера</p>', 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
